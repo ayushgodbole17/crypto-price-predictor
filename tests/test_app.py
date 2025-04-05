6bde7be6448aafae8a319a95e5a89861c644e292
Ayush Godbole <godbole.ayush@gmail.com>
2025-04-05 19:32:42 +0200

    testing implementation

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,3 +6,21 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json() == {"message": "Crypto Price Predictor is up!"}
+
+def test_predict_linear():
+    response = client.get("/predict?model_type=linear")
+    json_data = response.json()
+    assert response.status_code == 200
+    # Ensure response contains expected keys
+    for key in ["prediction", "training_rmse", "testing_rmse", "model_type"]:
+        assert key in json_data
+    assert json_data["model_type"] == "linear"
+
+def test_predict_rf():
+    response = client.get("/predict?model_type=rf")
+    json_data = response.json()
+    assert response.status_code == 200
+    for key in ["prediction", "training_rmse", "testing_rmse", "model_type"]:
+        assert key in json_data
+    assert json_data["model_type"] == "rf"
